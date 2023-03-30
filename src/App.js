725d7957f884c9f71c4eaff635c3b8a73a0de94f
Andrei Kondush <andrei.kondush@plus4u.net>
2023-03-30 17:23:48 +0200

    routing, browser-router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div class='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div class='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+                </div>
             </div>
-            {/*<Profile />*/}
-        </div>);
+            )
+        </BrowserRouter>)
 }
 
 export default App;
