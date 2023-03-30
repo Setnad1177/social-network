049015ebcaed004265af06cb288468bcd0b863b2
Andrei Kondush <andrei.kondush@plus4u.net>
2023-03-31 00:00:23 +0200

    Dialogs layout, navbar css small fix

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
-import s from './Dialogs.module.css';
+import React from "react";
+import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className="message">Hi</div>
+                <div className="message">How is your day?</div>
+                <div className="message">Yooo!</div>
+            </div>
+
         </div>
+
     )
 }
 
