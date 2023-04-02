9a9aa7300947dc473bb13a24846bcb76ab70c094
Andrei Kondush <andrei.kondush@plus4u.net>
2023-04-02 14:46:33 +0200

    Moving data aside

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,23 +16,40 @@ const Message = (props) => {
         <div className="message">{props.message}</div>
     )
 }
-
-
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: "Andrey"},
+        {id: 3, name: "Sveta"},
+        {id: 4, name: "Sasha"},
+        {id: 5, name: "Viktor"},
+        {id: 6, name: "Valera"}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'I\'ve learned my lesson. No more flaming people online unless it\'s 100% anonymously.'},
+        {id: 2, message: "See you in a few days. The house is going to take some cleaning."},
+        {id: 3, message: "Can't believe you're a movie producer! You're almost cool, Pop!"},
+        {id: 4, message: "Just let me know you're ok?"},
+        {id: 5, message: "I hope none of this stuff I'm seeing on the news has anything to do with you."},
+        {id: 6, message: "Are you guys on vacation? Haven't seen anyone at the house for a while? Just being a good neighbor! "}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Amanda De Santa" id="1"/>
-                <DialogItem name="Jimmy De Santa" id="2"/>
-                <DialogItem name="Lamar Davis" id="3"/>
-                <DialogItem name="Trevor Philips" id="4"/>
-                <DialogItem name="Franklin Clinton" id="5"/>
-                <DialogItem name="Denise Clinton" id="6"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name="Andrey" id="2"/>
+                <DialogItem name="Sveta" id="3"/>
+                <DialogItem name="Sasha" id="4"/>
+                <DialogItem name="Viktor" id="5"/>
+                <DialogItem name="Valera" id="6"/>
             </div>
             <div className={s.messages}>
-                <Message message="Will you clean the juicer after you use it?"/>
-                <Message message="Will you clean the juicer after you use it?"/>
-                <Message message="Will you clean the juicer after you use it?"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
             </div>
         </div>
     )
