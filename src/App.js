2faf38ee3aaf654e0d8fd9aeb45f1186cfafcf6e
Andrei Kondush <abrakadabro17@gmail.com>
2023-04-04 16:39:54 +0200

    Passing callback through props

--- src/App.js
+++ src/App.js
@@ -14,10 +14,12 @@ const App = (props) => {
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <div class='app-wrapper-content'>
+            <div className='app-wrapper-content'>
                 <Routes>
                     <Route path='/dialogs/*' element={<Dialogs state={props.state.dialogsPage}/>}/>
-                    <Route path='/profile' element={<Profile state={props.state.profilePage}/>}/>
+                    <Route path='/profile' element={<Profile
+                        state={props.state.profilePage}
+                        addPost={props.addPost}/>}/>
                     <Route path='/news' element={<News/>}/>
                     <Route path='/music' element={<Music/>}/>
                     <Route path='/settings' element={<Settings/>}/>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import state from './selfRedux/state';
+import {addPost} from './selfRedux/state';
 
-ReactDOM.render(<App state={state}/>, document.getElementById('root'));
+addPost('Hello world')
+
+ReactDOM.render(<App
+
+    state={state}
+    addPost={addPost}/>,
+
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
