a16df7dd549a9a841a40e9bc20c6d9895b21b10f
Andrei Kondush <abrakadabro17@gmail.com>
2023-04-10 12:44:12 +0200

    Сallback, subscribe, observer. Render - import export refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from "./render";
-import state from "./selfRedux/state";
+import state, {subscribe} from "./selfRedux/state";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost, updateNewPostText} from './selfRedux/state';
+
+
+
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+        , document.getElementById('root'));
+}
 
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
+
+
+
+
+
 
 
 // If you want your app to work offline and load faster, you can change
--- src/selfRedux/state.js
+++ src/selfRedux/state.js
@@ -1,37 +1,32 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log('State has been changed');
+}
 
 let state = {
     profilePage: {
         posts: [{id: 1, message: 'Hi, how are you?', likesCount: 12}, {
-            id: 2,
-            message: "It's my first post",
-            likesCount: 11
+            id: 2, message: "It's my first post", likesCount: 11
         }], newPostText: 'it-kama.com'
 
     }, dialogsPage: {
         messages: [{
-            id: 1,
-            message: 'I\'ve learned my lesson. No more flaming people online unless it\'s 100% anonymously.'
+            id: 1, message: 'I\'ve learned my lesson. No more flaming people online unless it\'s 100% anonymously.'
         }, {id: 2, message: "See you in a few days. The house is going to take some cleaning."}, {
-            id: 3,
-            message: "Can't believe you're a movie producer! You're almost cool, Pop!"
+            id: 3, message: "Can't believe you're a movie producer! You're almost cool, Pop!"
         }, {id: 4, message: "Just let me know you're ok?"}, {
-            id: 5,
-            message: "I hope none of this stuff I'm seeing on the news has anything to do with you."
+            id: 5, message: "I hope none of this stuff I'm seeing on the news has anything to do with you."
         }, {
             id: 6,
             message: "Are you guys on vacation? Haven't seen anyone at the house for a while? Just being a good neighbor! "
-        }],
-        dialogs: [{id: 1, name: 'Dimych'}, {id: 2, name: "Andrey"}, {id: 3, name: "Sveta"}, {
-            id: 4,
-            name: "Sasha"
+        }], dialogs: [{id: 1, name: 'Dimych'}, {id: 2, name: "Andrey"}, {id: 3, name: "Sveta"}, {
+            id: 4, name: "Sasha"
         }, {id: 5, name: "Viktor"}, {id: 6, name: "Valera"}]
     }
 }
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
     let newPost = {
         id: 5, message: state.profilePage.newPostText, likesCount: 0
     };
@@ -40,9 +35,14 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
 
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer; //observer (pattern)
+}
 export default state;
+
+// store - OOP
